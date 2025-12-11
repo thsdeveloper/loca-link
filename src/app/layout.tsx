e21ed887ad88fd91aee331ff7cc8562808f1048b
ThiagoSoares <ths.pereira@gmail.com>
2025-12-11 10:26:09 -0300

    feat: Add Hotjar tracking code to the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,6 +101,20 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar-tracking" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2280810,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
